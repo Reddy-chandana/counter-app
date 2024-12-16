7eab9b4bb85bb55b11c322e72c65f375c5bd93d5
Reddy chandana <chandana.reddy7708@gmail.com>
2024-12-16 19:57:15 +0530

    Add files via upload

---
+++ Counter.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    
+    return (
+        <div>
+            <h1>Counter</h1>
+            <div>
+                <span>{count}</span>
+            </div>
+            <div>
+                <button onClick={() => setCount(count + 1)}>+</button>
+                <button onClick={() => setCount(count - 1)}>-</button>
+                <button onClick={() => setCount(0)}>Reset</button>
+            </div>
+        </div>
+    );
+}
+
+export default Counter;
